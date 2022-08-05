b447aac94ad989181bd1b7507c762610d57b7661
sameeksha <sameekshasihare@gmail.com>
2022-08-05 16:49:38 +0530

    redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './app/store';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import employeeSlice from './features/employeeSlice';
 
+const store = configureStore({ 
+  reducer:{
+    employeeState:employeeSlice
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
